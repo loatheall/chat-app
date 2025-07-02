67dad6291d825e2ddeab857b0d1a3de2ff36966c
Maks <maksimkameko@gmail.com>
2025-07-02 21:37:54 +0200

    dodanie hasla bazy danych do server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const db = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'chatdb'
 });
 
